5593876b32b3443299f7f669c20cbbb17dfbfd32
Ko Shimizu <s.kou@nifty.com>
2014-02-04 01:22:54 +0900

    modify

--- awk/eom.sh
+++ awk/eom.sh
@@ -3,10 +3,10 @@
 
 echo "" | awk '
 function eomth(yyyymm){
-  yyyy=substr(yyyymm,1,4)
-  mm=substr(yyyymm,5,2) + 1
-  time = mktime(yyyy" "mm" 1 0 0 0") - 60 * 60 * 24 * 1
-  return strftime("%Y%m%d", time)
+    yyyy=substr(yyyymm,1,4)
+    mm=substr(yyyymm,5,2) + 1
+    time = mktime(yyyy" "mm" 1 0 0 0") - 60 * 60 * 24 * 1
+    return strftime("%Y%m%d", time)
 }
 {
    print eomth("201301") == "20130131" ? "." : "NG" # end of month 31th
