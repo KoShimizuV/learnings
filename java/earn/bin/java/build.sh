c61c3d70a7e7443cce843059ad5058d638742226
Ko Shimizu <s.kou@nifty.com>
2014-03-16 01:21:11 +0900

    add bz2, tar. modifed build.sh

--- java/earn/bin/java/build.sh
+++ java/earn/bin/java/build.sh
@@ -4,7 +4,7 @@ cd `dirname $0`
 . ~/git/esmon/bin/java/util.sh
 log "$0 $* Init"
 
-export CLASSPATH=`find lib -type f -name "*.jar" | tr '\n' ':' | sed "s/:$//g"`
+export CLASSPATH=`find lib -type f -name "*.jar" | paste -s -d:`
 echo $CLASSPATH
 
 find ./bin -type f | grep -v git | xargs -r rm -v
